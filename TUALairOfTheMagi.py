2e2683565e093362df91542dcf56b7951b8ab8ed
Ben Gardner <1bengardner@gmail.com>
2022-10-30 15:08:27 -0400

    Add some images to lair

--- TUALairOfTheMagi.py
+++ TUALairOfTheMagi.py
@@ -2,7 +2,7 @@
 File: TUALairOfTheMagi.py
 Author: Ben Gardner
 Created: October 27, 2022
-Revised: October 29, 2022
+Revised: October 30, 2022
 """
 
 
@@ -25,31 +25,66 @@ class LairOfTheMagi:
         self.movementVerb = "walk"
 
         plch = self.placeholder
+        entr = self.entrance
+        spid = self.spiderStairs
+        arch = self.archway
+        splt = self.split
+        dnLt = self.downLeft
+        dnRt = self.downRight
+        upLt = self.upLeft
+        upRt = self.upRight
+        noRt = self.noRight
+        noLt = self.noLeft
+        strU = self.stairsUp
+        strD = self.stairsDown
+        encL = self.enclaveLeft
+        encR = self.enclaveRight
+        encU = self.enclaveUp
+        noR2 = self.noRight2
+        noL2 = self.noLeft2
 
         self.spots = [
             [None, None, None, None, None, None, None, None, None, None, None],
             [None, None, None, None, None, plch, None, None, None, None, None],
             [None, None, None, None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, plch, None, None, None, None, None],
-            [None, None, None, None, plch, plch, plch, None, None, None, None],
-            [None, plch, None, plch, plch, None, plch, plch, None, plch, None],
-            [None, None, None, None, plch, plch, plch, None, None, None, None],
-            [None, None, None, None, None, plch, None, None, None, None, None],
+            [None, None, None, None, None, encU, None, None, None, None, None],
+            [None, None, None, None, dnRt, plch, dnLt, None, None, None, None],
+            [None, plch, None, encL, noR2, None, noL2, encR, None, plch, None],
+            [None, None, None, None, upRt, splt, upLt, None, None, None, None],
+            [None, None, None, None, None, strD, None, None, None, None, None],
             [None, None, None, None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, plch, None, None, None, None, None],
-            [None, None, None, None, plch, plch, plch, None, None, None, None],
-            [None, None, None, plch, plch, None, plch, plch, None, None, None],
-            [None, None, plch, plch, None, None, None, plch, plch, None, None],
-            [None, plch, plch, None, None, plch, None, None, plch, plch, None],
-            [None, None, plch, plch, None, None, None, plch, plch, None, None],
-            [None, None, None, plch, plch, None, plch, plch, None, None, None],
-            [None, None, None, None, plch, plch, plch, None, None, None, None],
-            [None, None, None, None, None, plch, None, None, None, None, None],
-            [None, None, None, None, None, plch, None, None, None, None, None],
-            [None, None, None, None, None, plch, None, None, None, None, None],
+            [None, None, None, None, None, strU, None, None, None, None, None],
+            [None, None, None, None, dnRt, plch, dnLt, None, None, None, None],
+            [None, None, None, dnRt, upLt, None, upRt, dnLt, None, None, None],
+            [None, None, dnRt, upLt, None, None, None, upRt, dnLt, None, None],
+            [None, plch, noRt, None, None, plch, None, None, noLt, plch, None],
+            [None, None, upRt, dnLt, None, None, None, dnRt, upLt, None, None],
+            [None, None, None, upRt, dnLt, None, dnRt, upLt, None, None, None],
+            [None, None, None, None, upRt, splt, upLt, None, None, None, None],
+            [None, None, None, None, None, arch, None, None, None, None, None],
+            [None, None, None, None, None, spid, None, None, None, None, None],
+            [None, None, None, None, None, entr, None, None, None, None, None],
             [None, None, None, None, None, None, None, None, None, None, None]]
 
-        self.encounters = {plch: {},
+        self.encounters = {
+            plch: {},
+            entr: {},
+            spid: {},
+            arch: {},
+            splt: {},
+            dnLt: {},
+            dnRt: {},
+            upLt: {},
+            upRt: {},
+            noRt: {},
+            noLt: {},
+            strU: {},
+            strD: {},
+            encL: {},
+            encR: {},
+            encU: {},
+            noR2: {},
+            noL2: {},
         }
 
     def movementActions(self):
@@ -68,5 +103,158 @@ class LairOfTheMagi:
     def placeholder(self, selectionIndex=None):
         self.view = "travel"
         self.menu = []
-        
+
+        return self.actions()
+
+    def entrance(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 0
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def spiderStairs(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 1
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def archway(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 2
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def split(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 3
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def downLeft(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 4
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def downRight(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 5
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def upLeft(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 6
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def upRight(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 7
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def noRight(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 8
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def noLeft(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 9
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def stairsUp(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 10
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def stairsDown(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 11
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def enclaveLeft(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 12
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def enclaveRight(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 13
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def enclaveUp(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 14
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def noRight2(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 15
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
+        return self.actions()
+
+    def noLeft2(self, selectionIndex=None):
+        self.view = "travel"
+        self.imageIndex = 16
+        self.text = None
+        self.helpText = None
+        self.menu = []
+
         return self.actions()
