ea65049cd3e37ddcb9e2c3caf32072f21f99f455
Ben Gardner <1bengardner@gmail.com>
2022-11-07 07:43:15 -0500

    Save music preferences
    - Forgot to add preferences class

---
+++ TUAPreferences.py
@@ -0,0 +1,13 @@
+"""
+File: TUAPreferences.py
+Author: Ben Gardner
+Created: November 5, 2022
+Revised: November 5, 2022
+"""
+
+
+class Preferences:
+
+    def __init__(self):
+        self.musicOn = True
+        self.sfxOn = True
