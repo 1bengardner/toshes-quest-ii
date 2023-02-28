be27eef275c292f82529c6abe429dd62c69be5d3
Ben Gardner <1bengardner@gmail.com>
2023-02-28 01:01:20 -0500

    Update all-rounder title to Omnibus

--- Toshe's Quest II.py
+++ Toshe's Quest II.py
@@ -5,7 +5,7 @@
 File: Toshe's Quest II.py
 Author: Ben Gardner
 Created: December 25, 2012
-Revised: January 3, 2023
+Revised: February 26, 2023
 """
 
 
@@ -350,7 +350,7 @@ class TopLeftFrame:
                     if str >= 50:
                         if dex >= 50:
                             if wis >= 50:
-                                return "Veteran"
+                                return "Omnibus"
                             return "Ranger"
                         elif wis >= 50:
                             return "Monk"
