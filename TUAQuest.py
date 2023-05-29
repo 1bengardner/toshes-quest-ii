5723c1a8f03387dd562e44bbb6205ce7319f070b
Ben Gardner <1bengardner@gmail.com>
2023-05-29 02:57:53 -0400

    Fix quest completion ticks

--- TUAQuest.py
+++ TUAQuest.py
@@ -3,7 +3,7 @@
 File: TUAQuest.py
 Author: Ben Gardner
 Created: November 17, 2022
-Revised: November 25, 2022
+Revised: May 29, 2023
 """
 
 class Quest:
@@ -32,14 +32,14 @@ class Quest:
         if 'Wildcard Kills' in self.COMPLETION_CRITERIA:
             targetNames = self.COMPLETION_CRITERIA['Wildcard Kills'][0]
             generalName = self.COMPLETION_CRITERIA['Wildcard Kills'][2]
-            startingKills = character.flags[self.START_FLAG] if isinstance(character.flags[self.START_FLAG], int) else 0
+            startingKills = character.flags[self.START_FLAG] if type(character.flags[self.START_FLAG]) is int else 0
             currentKills = sum(character.flags['Kills'][targetName] if targetName in character.flags['Kills'] else 0 for targetName in targetNames)
             if currentKills > startingKills:
                 details.append("%s: %s" % (generalName, "✗" * min(currentKills - startingKills, 10)))
             
         if 'Kills' in self.COMPLETION_CRITERIA:
             targetName = self.COMPLETION_CRITERIA['Kills'][0]
-            startingKills = character.flags[self.START_FLAG] if isinstance(character.flags[self.START_FLAG], int) else 0
+            startingKills = character.flags[self.START_FLAG] if type(character.flags[self.START_FLAG]) is int else 0
             currentKills = character.flags['Kills'][targetName] if targetName in character.flags['Kills'] else 0
             if currentKills > startingKills:
                 details.append("%s: %s" % (targetName.strip("123456789"), "✗" * min(currentKills - startingKills, 10)))
@@ -63,14 +63,14 @@ class Quest:
         if 'Wildcard Kills' in self.COMPLETION_CRITERIA:
             targetNames = self.COMPLETION_CRITERIA['Wildcard Kills'][0]
             targetKills = self.COMPLETION_CRITERIA['Wildcard Kills'][1]
-            startingKills = character.flags[self.START_FLAG] if isinstance(character.flags[self.START_FLAG], int) else 0
+            startingKills = character.flags[self.START_FLAG] if type(character.flags[self.START_FLAG]) is int else 0
             currentKills = sum(character.flags['Kills'][targetName] if targetName in character.flags['Kills'] else 0 for targetName in targetNames)
             completed = completed and currentKills - startingKills >= targetKills
         
         if 'Kills' in self.COMPLETION_CRITERIA:
             targetName = self.COMPLETION_CRITERIA['Kills'][0]
             targetKills = self.COMPLETION_CRITERIA['Kills'][1]
-            startingKills = character.flags[self.START_FLAG] if isinstance(character.flags[self.START_FLAG], int) else 0
+            startingKills = character.flags[self.START_FLAG] if type(character.flags[self.START_FLAG]) is int else 0
             currentKills = character.flags['Kills'][targetName] if targetName in character.flags['Kills'] else 0
             completed = completed and currentKills - startingKills >= targetKills
             
